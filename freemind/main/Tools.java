d78fd4ca8a3837b98540e4fd41836d97a9500e30
Clemens Luschnig <clemens.luschnig@gmail.com>
2015-06-14 15:13:51 +0200

    removed generateId from Tools

--- freemind/main/Tools.java
+++ freemind/main/Tools.java
@@ -566,32 +566,6 @@ public class Tools {
 				+ pPaper.getImageableWidth() + ";"
 				+ pPaper.getImageableHeight();
 	}
-
-	
-	public static String generateID(String proposedID, HashMap hashMap,
-			String prefix) {
-		String myProposedID = new String((proposedID != null) ? proposedID : "");
-		String returnValue;
-		do {
-			if (!myProposedID.isEmpty()) {
-				// there is a proposal:
-				returnValue = myProposedID;
-				// this string is tried only once:
-				myProposedID = "";
-			} else {
-				/*
-				 * The prefix is to enable the id to be an ID in the sense of
-				 * XML/DTD.
-				 */
-				returnValue = prefix
-						+ Integer.toString(Tools.ran.nextInt(2000000000));
-			}
-		} while (hashMap.containsKey(returnValue));
-		return returnValue;
-	}
-
-	
-
 	
 
 }
